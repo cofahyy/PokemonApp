481b8b871a841de9130f643981d5589bbe0b48d6
ckbarts <ckbarts@iu.edu>
2025-12-14 19:43:36 -0500

    spinner

--- app/src/main/java/edu/iu/luddy/c323_capstone/AboutActivity.kt
+++ app/src/main/java/edu/iu/luddy/c323_capstone/AboutActivity.kt
@@ -1,11 +1,10 @@
 package edu.iu.luddy.c323_capstone
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-
-// About
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +14,19 @@ class AboutActivity : AppCompatActivity() {
         val btnBack = findViewById<Button>(R.id.btnBack)
         btnBack.setOnClickListener {
             finish()
+        }
+
+        val spnPokemonFacts = findViewById<Spinner>(R.id.spnPokemonFacts)
+        val pokemonFacts = resources.getStringArray(R.array.pokemon_facts)
+
+        val adapter = ArrayAdapter(this, R.layout.custom_spinner_item, pokemonFacts)
+        adapter.setDropDownViewResource(R.layout.custom_spinner_item)
+        spnPokemonFacts.adapter = adapter
+
+        // randomize
+        if (pokemonFacts.isNotEmpty()) {
+            val randomIndex = (pokemonFacts.indices).random()
+            spnPokemonFacts.setSelection(randomIndex)
         }
     }
 }
